ca5a17e2f3b7855014315de1093cf395afacc776
Gustavo Verneck <gustavoverneck@hotmail.com>
2025-05-16 17:04:17 -0300

    structure org

--- numenit_demo_ams/wsgi.py
+++ numenit_demo_ams/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for numenit project.
+WSGI config for numenit_demo_ams project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'numenit.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'numenit_demo_ams.settings')
 
 application = get_wsgi_application()
